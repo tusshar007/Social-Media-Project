3d1c71cb949d48c09418ffaec26c2f02ae401105
tusshar05 <tushar.joshi789@gmail.com>
2019-02-05 19:53:32 +0530

    Login Pages

--- Social/accounts/views.py
+++ Social/accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.core.urlresolvers import reverse_lazy
 # Create your views here.
+from django.views.generic import CreateView
+from . import forms
+
+class Signup(CreateView):
+    form_class = forms.UserCreateForm
+    success_url = reverse_lazy('login')
+    template_name = 'accounts/signup.html'
